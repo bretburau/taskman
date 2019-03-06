553fcf3d7706b982fa5e6756fcc00198dfd813ee
Bret <Burhooo@gmail.com>
2019-03-06 07:13:57 -0600

    setup task controller methods

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -6,5 +6,23 @@ use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate(['title' => 'required']);
+        
+        $task = Task::create([
+            'title' => $validatedData['title'],
+            'project_id' => $request->project_id
+        ]);
+
+        return $task->toJson();
+    }
+
+    public function markAsCopleted(Task $task)
+    {
+        $task->is_completed = true;
+        $task->update();
+
+        return response()->json('Task updated!');
+    }
 }
